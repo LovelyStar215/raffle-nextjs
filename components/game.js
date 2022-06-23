3133722473c146b6e428b6044c456f2291798379
LovelyStar215 <ericjin106@gmail.com>
2022-06-23 18:42:33 +0100

    Message for players with no tickets in game

--- components/game.js
+++ components/game.js
@@ -173,47 +173,58 @@ const Game = ({
 		let ticketItems = '';
 		let totalSharePercentage = 0;
 		let totalShareValue, totalSharePercentageString;
-		if (gameTickets && gameTickets[activeAddress]) {
-			ticketItems = gameTickets[activeAddress].map((val, key) => {
-				return (
-					<div className="ticket" key={`game-${game.gameNumber}-ticket-${key}`}>
-						<span>#{val}</span>
+
+		if (!gameTickets || !gameTickets[activeAddress] || !gameTickets[activeAddress].length) {
+			return (
+				<div className="result">
+					<div className="panels">
+						<div>
+							<p>You currently don't hold any tickets in this game.</p>
+						</div>
 					</div>
-				);
-			}, {});
+				</div>
+			);
+		}
 
-			// Players current pot share (value and percentage), in play
-			// let token = getToken(game.tokenAddress);
-			if (gameTokenMetadata) {
-				console.log('game.ticketPrice');
-				console.log(game.ticketPrice);
-				console.log('ticketCount');
-				let playerTicketCount = gameTickets[activeAddress].length;
-				let playerTicketCountBN = web3.utils.toBN(playerTicketCount);
-				// console.log(playerTicketCount);
-				// let decimals = web3.utils.toBN(gameTokenMetadata.decimals);
-				// console.log('gameTokenMetadata.decimals');
-				// console.log(decimals);
-				// totalShareValue = web3.utils
-				// 	.toBN(game.ticketPrice)
-				// 	.mul(playerTicketCount);
-				// console.log('shareBN');
-				// console.log(totalShareValue.toString());
-				// totalShareValue = web3.utils
-				// 	.toBN(game.ticketPrice).div(
-				// 		web3.utils
-				// 		.toBN(10)
-				// 		.pow(decimals)
-				// 	)
-				// 	.toString() + ' ' + gameTokenMetadata.symbol;
-				totalShareValue = web3.utils.fromWei(web3.utils.toBN(game.ticketPrice).mul(playerTicketCountBN));
-				if (gameTokenMetadata.symbol) totalShareValue += ' ' + gameTokenMetadata.symbol;
+		ticketItems = gameTickets[activeAddress].map((val, key) => {
+			return (
+				<div className="ticket" key={`game-${game.gameNumber}-ticket-${key}`}>
+					<span>#{val}</span>
+				</div>
+			);
+		}, {});
 
-				let gameTicketCount = parseInt(game.ticketCount);
-				if (gameTicketCount)
-					totalSharePercentage = ((playerTicketCount * 100) / gameTicketCount).toFixed(2);
-				totalSharePercentageString = `(${totalSharePercentage}%)`;
-			}
+		// Players current pot share (value and percentage), in play
+		// let token = getToken(game.tokenAddress);
+		if (gameTokenMetadata) {
+			console.log('game.ticketPrice');
+			console.log(game.ticketPrice);
+			console.log('ticketCount');
+			let playerTicketCount = gameTickets[activeAddress].length;
+			let playerTicketCountBN = web3.utils.toBN(playerTicketCount);
+			// console.log(playerTicketCount);
+			// let decimals = web3.utils.toBN(gameTokenMetadata.decimals);
+			// console.log('gameTokenMetadata.decimals');
+			// console.log(decimals);
+			// totalShareValue = web3.utils
+			// 	.toBN(game.ticketPrice)
+			// 	.mul(playerTicketCount);
+			// console.log('shareBN');
+			// console.log(totalShareValue.toString());
+			// totalShareValue = web3.utils
+			// 	.toBN(game.ticketPrice).div(
+			// 		web3.utils
+			// 		.toBN(10)
+			// 		.pow(decimals)
+			// 	)
+			// 	.toString() + ' ' + gameTokenMetadata.symbol;
+			totalShareValue = web3.utils.fromWei(web3.utils.toBN(game.ticketPrice).mul(playerTicketCountBN));
+			if (gameTokenMetadata.symbol) totalShareValue += ' ' + gameTokenMetadata.symbol;
+
+			let gameTicketCount = parseInt(game.ticketCount);
+			if (gameTicketCount)
+				totalSharePercentage = ((playerTicketCount * 100) / gameTicketCount).toFixed(2);
+			totalSharePercentageString = `(${totalSharePercentage}%)`;
 		}
 
 		return (
@@ -238,7 +249,6 @@ const Game = ({
 				</div>
 			</div>
 		)
-		
 	};
 
 	const gameStateBanner = () => {
